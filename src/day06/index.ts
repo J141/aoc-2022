c009015e38250090f19cece279b3a7905ff3f33e
J141 <j.dekoning141@gmail.com>
2022-12-07 01:17:29 +0100

    add day 6 solutions

---
+++ src/day06/index.ts
@@ -0,0 +1,47 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput.split('');
+
+const solve = (input: string[], markerLength: number) => {
+  const buffer = input.slice(0, markerLength - 1);
+  for(let i = 3; i < input.length; i++) {
+    if([...buffer, input[i]].every((c, i, arr) => arr.indexOf(c) === i))
+      return i + 1;
+
+    buffer.shift();
+    buffer.push(input[i]);
+  }
+}
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  return solve(input, 4)
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  return solve(input, 14);
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
